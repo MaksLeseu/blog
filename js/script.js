c27fc7851e5b59c05954aadf1411f860db575904
MaksLeseu <117761891+MaksLeseu@users.noreply.github.com>
2022-11-15 13:13:34 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,10 +1,112 @@
 "use strict"
 
+
 const MenuBurger = document.querySelector('.header__burger');
 const HeaderMenu = document.querySelector('.header__menu');
 const Sidebar = document.querySelector('.sidebar');
+const Container = document.querySelector('.container');
+const Body = document.querySelector('body');
+
 MenuBurger.addEventListener("click", function(e) {
     MenuBurger.classList.toggle('_active-burger');
     HeaderMenu.classList.toggle('_active-burger');
     Sidebar.classList.toggle('_active-burger');
+    Container.classList.toggle('_active-burger');
+    Body.classList.toggle('_active-burger');
+    
 });
+
+const SidebarLinkMenu = document.querySelector('.sidebar__link-menu');
+const SidebarMenu = document.querySelector('.sidebar__menu');
+const SidebarSpan = document.querySelector('.sidebar__link-span');
+SidebarLinkMenu.addEventListener("click", function(e) {
+    SidebarLinkMenu.classList.toggle('_active-menu');
+    SidebarMenu.classList.toggle('_active-menu');
+    SidebarSpan.classList.toggle('_active-menu');
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
