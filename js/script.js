c469daa037f7748e5441d291116fff29c0cf0663
Maks Leseu <lesov0iblr@gmail.com>
2022-11-28 13:00:34 +0100

    Burger-menu section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 "use strict"
 
+const MenuBurger = document.querySelector('.header__burger');
+const Sidebar = document.querySelector('.sidebar');
+const Container = document.querySelector('.container');
+const Body = document.querySelector('body');
+MenuBurger.addEventListener("click", function(e) {
+    MenuBurger.classList.toggle('_active-burger');
+    Sidebar.classList.toggle('_active-burger');
+    Container.classList.toggle('_active-burger');
+    Body.classList.toggle('_active-burger');
+    
+});
+
+const SidebarLinkMenu = document.querySelector('.sidebar__link-menu');
+const SidebarMenu = document.querySelector('.sidebar__menu');
+const SidebarSpan = document.querySelector('.sidebar__link-span');
+SidebarLinkMenu.addEventListener("click", function(e) {
+    SidebarLinkMenu.classList.toggle('_active-menu');
+    SidebarMenu.classList.toggle('_active-menu');
+    SidebarSpan.classList.toggle('_active-menu');
+});
+
 new Swiper('.gallery-slider',{
     navigation: {
         nextEl: '.swiper-button-next',
@@ -29,31 +50,6 @@ new Swiper('.gallery-slider',{
     },
     speed: 800,
 });
-
-
-
-const MenuBurger = document.querySelector('.header__burger');
-const Sidebar = document.querySelector('.sidebar');
-const Container = document.querySelector('.container');
-const Body = document.querySelector('body');
-MenuBurger.addEventListener("click", function(e) {
-    MenuBurger.classList.toggle('_active-burger');
-    Sidebar.classList.toggle('_active-burger');
-    Container.classList.toggle('_active-burger');
-    Body.classList.toggle('_active-burger');
-    
-});
-
-const SidebarLinkMenu = document.querySelector('.sidebar__link-menu');
-const SidebarMenu = document.querySelector('.sidebar__menu');
-const SidebarSpan = document.querySelector('.sidebar__link-span');
-SidebarLinkMenu.addEventListener("click", function(e) {
-    SidebarLinkMenu.classList.toggle('_active-menu');
-    SidebarMenu.classList.toggle('_active-menu');
-    SidebarSpan.classList.toggle('_active-menu');
-});
-
-
 
 
 
